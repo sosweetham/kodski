3ac657a2eb48c0b47e313f693775d841c8e751cc
SoSweetHam <sosweetham@gmail.com>
2026-02-25 11:26:30 +0530

    fix: toc positioning

--- app/src/routes/blog/[slug]/+page.svelte
+++ app/src/routes/blog/[slug]/+page.svelte
@@ -18,7 +18,7 @@
 	<li>{data.meta.title}</li>
 </ol>
 
-<article class="relative">
+<article>
   <hgroup>
     <h1 class="h1 text-5xl mb-2">{data.meta.title}</h1>
     <p>Published at {(new Date(data.meta.date)).toLocaleDateString()}</p>
@@ -32,8 +32,12 @@
     {data.meta.description}
   </p>
   <hr class='my-2' />
-  <TableOfContents class="card absolute top-1/2 -translate-y-1/2 right-0 p-4 text-inherit hidden md:block max-h-[300px] overflow-scroll overflow-x-hidden hide-scrollbar" />
-  <div class="prose text-inherit mt-2" use:tocCrawler={{ mode: 'generate', scrollTarget: '#page' }}>
-    <svelte:component this={data.content} />
+  <div class="flex gap-16 lg:gap-24 mt-2 justify-between">
+    <div class="prose text-inherit flex-1 min-w-0 max-w-3xl" use:tocCrawler={{ mode: 'generate', scrollTarget: '#page' }}>
+      <svelte:component this={data.content} />
+    </div>
+    <aside class="hidden md:block w-56 shrink-0">
+      <TableOfContents class="card sticky top-24 p-4 text-inherit max-h-[calc(100vh-8rem)] overflow-y-auto overflow-x-hidden hide-scrollbar" />
+    </aside>
   </div>
 </article>
